5f95564a03dc8ad2e4f35b2429fdce916000891c
farshadmoosavi <97333681+farshadmoosavi@users.noreply.github.com>
2023-04-24 11:24:51 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './styles/app.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Song from "./components/Song";
+import Player from "./components/Player";
+import data from "./data"
 
 function App() {
+  const [songs, setSongs] = useState(data());  //songs list
+  const [currentSong, setCurrentSong] = useState(songs[0]);  // the song which is currently playing
+  const [isPlaying, setIsPlaying] = useState(false);  // we should pass this state into the player to find out playing or not
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Music player</h1>
+      <Song currentSong={currentSong} />   {/* pass the props into song.js */}
+      <Player currentSong={currentSong} isPlaying={isPlaying} setIsPlaying={setIsPlaying}/>  {/* pass the props into player.js */}
     </div>
   );
 }
