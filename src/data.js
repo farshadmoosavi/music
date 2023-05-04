458828cca274e264a4d2d122c5ce4d1c004efeb4
farshadmoosavi <97333681+farshadmoosavi@users.noreply.github.com>
2023-05-04 15:16:33 +0330

    update

--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ export default function musicData() {
             cover: "https://chillhop.com/wp-content/uploads/2023/04/a32d2ec47ba20c985c2b2425fd24d277a8f36d3e-1024x1024.jpg",
             artist: " Aviino",
             audio: "https://mp3.chillhop.com/serve.php/?mp3=55310",
-            color: ["E9C894", "352022"],
+            color: ["#E9C894", "#352022"],
             id: uuidv4(),
             active: true
 
@@ -17,7 +17,7 @@ export default function musicData() {
             cover: "https://chillhop.com/wp-content/uploads/2023/02/492202568b0d18312224043686735f7f1a47f67b-1024x1024.jpg",
             artist: " Plusma",
             audio: "https://mp3.chillhop.com/serve.php/?mp3=49369",
-            color: ["CCFBFB", "D4C65C"],
+            color: ["#CCFBFB", "#D4C65C"],
             id: uuidv4(),
             active: false
         },
@@ -26,7 +26,7 @@ export default function musicData() {
             cover: "https://chillhop.com/wp-content/uploads/2022/04/73f23095038f6f023083520706f3e5345f5dd682-1024x1024.jpg",
             artist: " Yasper",
             audio: "https://mp3.chillhop.com/serve.php/?mp3=32860",
-            color: ["60B7DF", "EE81B3"],
+            color: ["#60B7DF", "#EE81B3"],
             id: uuidv4(),
             active: false
         },
@@ -55,7 +55,7 @@ export default function musicData() {
             cover: "https://chillhop.com/wp-content/uploads/2022/05/d8344de22563af8eaec8f544c14711592eabef26-1024x1024.jpg",
             artist: " Guillaume Muschalle",
             audio: "https://mp3.chillhop.com/serve.php/?mp3=28895",
-            color: ["FFFEC6", "534D4F"],
+            color: ["#FFFEC6", "#534D4F"],
             id: uuidv4(),
             active: false
         }
@@ -65,7 +65,7 @@ export default function musicData() {
             cover: "https://chillhop.com/wp-content/uploads/2022/05/d8344de22563af8eaec8f544c14711592eabef26-1024x1024.jpg",
             artist: " Guillaume Muschalle",
             audio: "https://mp3.chillhop.com/serve.php/?mp3=28895",
-            color: ["FFFEC6", "534D4F"],
+            color: ["#FFFEC6", "#534D4F"],
             id: uuidv4(),
             active: false
         }
@@ -75,7 +75,7 @@ export default function musicData() {
             cover: "https://chillhop.com/wp-content/uploads/2022/05/d8344de22563af8eaec8f544c14711592eabef26-1024x1024.jpg",
             artist: " Guillaume Muschalle",
             audio: "https://mp3.chillhop.com/serve.php/?mp3=28895",
-            color: ["FFFEC6", "534D4F"],
+            color: ["#FFFEC6", "#534D4F"],
             id: uuidv4(),
             active: false
         }
@@ -85,7 +85,7 @@ export default function musicData() {
             cover: "https://chillhop.com/wp-content/uploads/2022/05/d8344de22563af8eaec8f544c14711592eabef26-1024x1024.jpg",
             artist: " Guillaume Muschalle",
             audio: "https://mp3.chillhop.com/serve.php/?mp3=28895",
-            color: ["FFFEC6", "534D4F"],
+            color: ["#FFFEC6", "#534D4F"],
             id: uuidv4(),
             active: false
         }
